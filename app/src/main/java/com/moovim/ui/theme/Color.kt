6bb851a843cd4c51f832cf641583fe20bb5cddb7
Juan Burda <jburda@itba.edu.ar>
2022-11-10 23:36:22 -0300

    new: theme colors and typography

--- app/src/main/java/com/moovim/ui/theme/Color.kt
+++ app/src/main/java/com/moovim/ui/theme/Color.kt
@@ -2,7 +2,7 @@ package com.moovim.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val Red500 = Color(0xFFBF3D3D)
+val Gray200 = Color(0xFF252525)
+val Black500 = Color(0xFF181818)
+val Yellow500 = Color(0xFFBF9B3D)
--- app/src/main/java/com/moovim/ui/theme/Type.kt
+++ app/src/main/java/com/moovim/ui/theme/Type.kt
@@ -2,17 +2,53 @@ package com.moovim.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.moovim.R
+
+private val Inter= FontFamily(
+        Font(R.font.inter_light, FontWeight.W300),
+        Font(R.font.inter_regular, FontWeight.W400),
+        Font(R.font.inter_medium, FontWeight.W500),
+        Font(R.font.inter_bold, FontWeight.W600),
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
+        h1 = TextStyle(
+                fontFamily = Inter,
+                fontWeight = FontWeight.W500,
+                fontSize = 30.sp
+        ),
+        h2 = TextStyle(
+                fontFamily = Inter,
+                fontWeight = FontWeight.W500,
+                fontSize = 24.sp)
+        ,
+        h3 = TextStyle(
+                fontFamily = Inter,
+                fontWeight = FontWeight.W500,
+                fontSize = 20.sp),
+        h4 = TextStyle(
+                fontFamily = Inter,
+                fontWeight = FontWeight.W400,
+                fontSize = 18.sp),
+        h5 = TextStyle(
+                fontFamily = Inter,
+                fontWeight = FontWeight.W400,
+                fontSize = 16.sp),
+        h6 = TextStyle(
+                fontFamily = Inter,
+                fontWeight = FontWeight.W400,
+                fontSize = 14.sp),
         body1 = TextStyle(
-                fontFamily = FontFamily.Default,
+                fontFamily = Inter,
                 fontWeight = FontWeight.Normal,
-                fontSize = 16.sp
-        )
+                fontSize = 16.sp)
+
+
         /* Other default text styles to override
     button = TextStyle(
         fontFamily = FontFamily.Default,
